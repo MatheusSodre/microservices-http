4ad61aaa3269002e9be3abf57fba78e9ad22f769
MatheusSodre <matheus.sodree@gmail.com>
2023-06-19 16:24:21 -0300

    Consumer

--- src/Services/Traits/ConsumeExternalService.php
+++ src/Services/Traits/ConsumeExternalService.php
@@ -1,5 +1,28 @@
 <?php 
 namespace Matheus\MicroservicesHttp\Services\Traits;
+use Illuminate\Support\Facades\Http;
+
+trait ConsumeExternalService
+{
+    public function headers(array $headers = [])
+    {
+        array_push($headers,[
+            'Accept' => 'aplication/json',
+            'Authorization' => $this->token,
+        ]);
+        return Http::withHeaders($headers);
+    }
+    public function request(
+        string $method,
+        string $endPoint,
+        array  $formParams = [],
+        array  $headers = []
+    ){
+      return $this->headers($headers)
+                  ->$method($this->url . $endPoint,$formParams);
+    }
+}
+
 
 
 ?>
