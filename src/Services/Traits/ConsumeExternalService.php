ecb2078bdab4afb0012fc36eef887d641fade806
MatheusSodre <matheus.sodree@gmail.com>
2023-06-21 12:51:29 -0300

    A

--- src/Services/Traits/ConsumeExternalService.php
+++ src/Services/Traits/ConsumeExternalService.php
@@ -4,25 +4,16 @@ use Illuminate\Support\Facades\Http;
 
 trait ConsumeExternalService
 {
-    public function headers(array $headers = [])
-    {
-        array_push($headers,[
-            'Accept' => 'aplication/json',
-            'Authorization' => $this->token,
-        ]);
-        return Http::withHeaders($headers);
-    }
     public function request(
         string $method,
         string $endPoint,
         array  $formParams = [],
         array  $headers = []
     ){
-      return $this->headers($headers)
+      $header = ['Accept' => 'aplication/json',
+                 'Authorization' => $this->token];  
+      return Http::withHeaders()
                   ->$method($this->url . $endPoint,$formParams);
     }
 }
-
-
-
 ?>
